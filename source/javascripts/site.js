b9b562246ccfd262e8bde6174d314a34b0363c94
Richard Lindsay <richard@tictocfamily.com>
2018-12-03 17:01:59 +0000

    Automatically check if Falkirk won.

---
+++ source/javascripts/site.js
@@ -0,0 +1,35 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://www.footballwebpages.co.uk/fixtures-results.json?team=343&results=1&fixtures=0');
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      setTimeout(function() {
+        var result = JSON.parse(xhr.responseText);
+
+        var homeTeamNumber = result.matchesTeam.match[0].homeTeamNo;
+        var homeTeamScore = result.matchesTeam.match[0].homeTeamScore;
+        var awayTeamScore = result.matchesTeam.match[0].awayTeamScore;
+
+        if (homeTeamNumber === 343) {
+          if (homeTeamScore > awayTeamScore) {
+            document.querySelector('h1').innerText = 'Yes.';
+          }
+          else {
+            document.querySelector('h1').innerText = 'No.';
+          }
+        }
+        else {
+          if (awayTeamScore > homeTeamScore) {
+            document.querySelector('h1').innerText = 'Yes.';
+          }
+          else {
+            document.querySelector('h1').innerText = 'No.';
+          }
+        }
+      }, 2000);
+    }
+  };
+  xhr.send();
+});
