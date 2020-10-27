b298e7d074b366f60731b3809532f379bdfbf0b5
Richard Lindsay <richard@tictocfamily.com>
2020-10-27 14:42:26 +0000

    Update to use api

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,32 +2,12 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://www.footballwebpages.co.uk/fixtures-results.json?team=343&results=1&fixtures=0');
+  xhr.open('GET', 'https://did-falkirk-win-api.herokuapp.com/api/v1');
   xhr.onload = function() {
     if (xhr.status === 200) {
       setTimeout(function() {
         var result = JSON.parse(xhr.responseText);
-
-        var homeTeamNumber = result.matchesTeam.match[0].homeTeamNo;
-        var homeTeamScore = result.matchesTeam.match[0].homeTeamScore;
-        var awayTeamScore = result.matchesTeam.match[0].awayTeamScore;
-
-        if (homeTeamNumber == 343) {
-          if (homeTeamScore > awayTeamScore) {
-            document.querySelector('h1').innerText = 'Yes.';
-          }
-          else {
-            document.querySelector('h1').innerText = 'No.';
-          }
-        }
-        else {
-          if (awayTeamScore > homeTeamScore) {
-            document.querySelector('h1').innerText = 'Yes.';
-          }
-          else {
-            document.querySelector('h1').innerText = 'No.';
-          }
-        }
+        document.querySelector('h1').innerText = result.message
       }, 2000);
     }
   };
