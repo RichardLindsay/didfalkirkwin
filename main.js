543f55e3f54aef883f0130226e2ec69466de0ae8
Richard Lindsay <richard@tictocfamily.com>
2023-02-14 11:08:55 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://cpug0736ma.execute-api.us-east-1.amazonaws.com/api');
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      setTimeout(function() {
+        var result = JSON.parse(xhr.responseText);
+        document.querySelector('h1').innerText = result.message
+      }, 2000);
+    }
+  };
+  xhr.send();
+});
